b50898738bbd09fbb271368399e2a42edd0daf10
alejandra.florencia@gmail.com <alejandra.florencia@gmail.com@d609b031-9a46-e17f-fe34-24eb95090216>
2014-04-14 00:50:37 +0000

    comentario agregado nuevo test

--- trunk/test/arbolBTest.cpp
+++ trunk/test/arbolBTest.cpp
@@ -1,10 +1,14 @@
 #include "gtest/gtest.h"
 #include "arbolB.h"
- 
+
 TEST(SampleTest, AssertionTrue) {
     ASSERT_EQ(1, 1);
 }
 
 TEST(SumaTest, ExpectTrue) {
     EXPECT_EQ(5, Suma(2, 3));
+}
+
+TEST(SumaTest2, ExpectTrue) {
+    EXPECT_EQ(6, Suma(3, 3));
 }
